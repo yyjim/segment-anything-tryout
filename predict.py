69aa97d94f35be567edac2be048515ab797b5c9b
yyjim <yy.jim731@gmail.com>
2023-04-14 11:07:53 +0800

    feat: basic segment anything with box demo

--- predict.py
+++ predict.py
@@ -1,29 +1,23 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
+import os
 import imutils
+from typing import List
 import numpy as np
 import cv2
 from cog import BasePredictor, Input, Path
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 import sys
-sys.path.append("..")
+import ast
+
+os.system(
+    "wget - q https: // dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth")
 
 
-def show_anns(anns):
-    if len(anns) == 0:
-        return
-    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
-
-    ref = anns[0]['segmentation']
-
-    canvas = np.zeros((ref.shape[0], ref.shape[1], 3))
-
-    for ann in sorted_anns:
-        m = ann['segmentation']
-        color_mask = np.random.random((1, 3))
-        canvas[m] = np.uint8(color_mask*255)
-    return canvas
+def sort_by_indexes(lst, indexes, reverse=False):
+    return [val for (_, val) in sorted(zip(indexes, lst), key=lambda x:
+            x[0], reverse=reverse)]
 
 
 class Predictor(BasePredictor):
@@ -38,43 +32,64 @@ class Predictor(BasePredictor):
     def predict(
         self,
         image: Path = Input(description="Input image"),
-        resize_width: int = Input(
-            default=1024, description="The width to resize the image to before running inference."),
-        points_per_side: int = Input(
-            default=32, description="The number of points to be sampled along one side of the image. The total number of points is points_per_side**2. If None, point_grids must provide explicit point sampling."),
-        pred_iou_thresh: float = Input(
-            default=0.88, description="A filtering threshold in [0,1], using the model's predicted mask quality."),
-        stability_score_thresh: float = Input(
-            default=0.95, description="A filtering threshold in [0,1], using the stability of the mask under changes to the cutoff used to binarize the model's mask predictions."),
-        stability_score_offset: float = Input(
-            default=1.0, description="The amount to shift the cutoff when calculated the stability score."),
-        box_nms_thresh: float = Input(
-            default=0.7, description="The box IoU cutoff used by non-maximal suppression to filter duplicate masks."),
-        crop_n_layers: int = Input(
-            default=0, description="If >0, mask prediction will be run again on crops of the image. Sets the number of layers to run, where each layer has 2**i_layer number of image crops"),
-        crop_nms_thresh: float = Input(
-            default=0.7, description="The box IoU cutoff used by non-maximal suppression to filter duplicate masks between different crops."),
-        crop_overlap_ratio: float = Input(
-            default=512 / 1500, description="Sets the degree to which crops overlap. In the first crop layer, crops will overlap by this fraction of the image length. Later layers with more crops scale down this overlap."),
-        crop_n_points_downscale_factor: int = Input(
-            default=1, description="The number of points-per-side sampled in layer n is scaled down by crop_n_points_downscale_factor**n."),
-        min_mask_region_area: int = Input(
-            default=0, description="If >0, postprocessing will be applied to remove disconnected regions and holes in masks with area smaller than min_mask_region_area."),
-    ) -> Path:
+        box: str = Input(
+            default=None, description="Bounding box coordinates [x, y, w, h]. If None, the entire image will be used."),
+        multimask_output: bool = Input(
+            default=False, description="If True, the output will be a list of masks. If False, the output will be a single mask."),
+    ) -> List[Path]:
         """Run a single prediction on the model"""
-        args = locals()
-        del args["self"]
-        del args["image"]
-        del args["resize_width"]
 
-        mask_generator = SamAutomaticMaskGenerator(self.sam, **args)
+        # convert the string to a Python list
+        try:
+            # try to convert the string to a Python list
+            array = ast.literal_eval(box)
+            boxArray = np.array([
+                array[0],
+                array[1],
+                array[0] + array[2],
+                array[1] + array[3]
+            ])
+        except (SyntaxError, ValueError):
+            # if it's not a valid Python expression, use the default array
+            boxArray = None
 
-        image = cv2.imread(str(image))
-        image = imutils.resize(image, width=resize_width)
+        # setups the predictor and image
+        mask_predictor = SamPredictor(self.sam)
+        image_bgr = cv2.imread(str(image))
+        image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
 
-        masks = mask_generator.generate(image)
-        annotations = show_anns(masks)
+        # run the prediction
+        mask_predictor.set_image(image_rgb)
+        masks, scores, logits = mask_predictor.predict(
+            box=boxArray,
+            multimask_output=multimask_output
+        )
 
-        cv2.imwrite("output_mask.png", annotations)
+        # sort the masks by score
+        sorted_masks = sort_by_indexes(masks, scores, reverse=True)
 
-        return Path("output_mask.png")
+        print(scores)
+        # create mask images
+        color = np.array([1, 1, 1])
+        results = []
+
+        for index, item in enumerate(sorted_masks):
+            filename = f"mask.{index}.png"
+            mask = sorted_masks[index]
+            h, w = mask.shape[-2:]
+            mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+            mask_image = (mask_image * 255).astype(np.uint8)
+            #mask_image = cv2.bitwise_not(mask_image)
+            cv2.imwrite(filename, mask_image)
+            results.append(Path(filename))
+
+        # create the output image
+        output_mask = cv2.imread("mask.0.png")
+        output_mask = cv2.cvtColor(output_mask, cv2.COLOR_BGR2GRAY)
+        b, g, r = cv2.split(image_bgr)
+        output_image = cv2.merge([b, g, r, output_mask], 4)
+        print(output_image.shape)
+        cv2.imwrite('output.png', output_image)
+        results.append(Path('output.png'))
+        print(results)
+        return results
